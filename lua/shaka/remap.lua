99334b978642ef3d228b85a9bf91910ef5f7a4dc
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2024-01-27 12:07:14 -0300

    remap: add remaps for buffer (navigation, creation, moving etc)

--- lua/shaka/remap.lua
+++ lua/shaka/remap.lua
@@ -3,6 +3,7 @@ local keymap = vim.keymap
 local cmd = vim.cmd
 
 g.mapleader = " "
+keymap.set("n", "<leader>so", cmd.so)
 keymap.set("n", "<leader>po", cmd.Ex)
 keymap.set("n", "<leader>pc", cmd.Rex)
 keymap.set("n", "<leader>w", cmd.w)
@@ -10,5 +11,25 @@ keymap.set("n", "<leader>q", cmd.q)
 keymap.set("n", "<leader>bd", cmd.bd)
 keymap.set("n", "<leader>so", cmd.so)
 keymap.set("n", "<leader>noh", cmd.noh)
-keymap.set("n", "<leader>t", ":20sp term://fish<CR>")
+keymap.set("n", "<leader>t", ":10sp term://fish<CR>")
 
+--- Buffers
+-- Creation
+keymap.set("n", "<leader>vs", cmd.vs)
+keymap.set("n", "<leader>sp", cmd.sp)
+
+-- Navigation
+keymap.set("n", "<leader>wh", "<C-W><C-H>")
+keymap.set("n", "<leader>wj", "<C-W><C-J>")
+keymap.set("n", "<leader>wk", "<C-W><C-K>")
+keymap.set("n", "<leader>wl", "<C-W><C-L>")
+
+-- Moving windows
+keymap.set("n", "wmh", "<C-W><S-H>")
+keymap.set("n", "wmj", "<C-W><S-J>")
+keymap.set("n", "wmk", "<C-W><S-K>")
+keymap.set("n", "wml", "<C-W><S-L>")
+
+--- Terminal
+
+keymap.set("t", "<Esc>", "<C-\\><C-n>")

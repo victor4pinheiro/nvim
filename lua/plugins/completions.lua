32e5d0d51008e0a3d37869c5f3b5c29568a5fd4b
victor4pinheiro <victor.pinheiro.soft@proton.me>
2025-01-17 11:06:32 -0300

    replace cmp by blink-cmp (it supports cmp sources)

---
+++ lua/plugins/completions.lua
@@ -0,0 +1,21 @@
+return {
+    'saghen/blink.cmp',
+    lazy = false,
+    -- optional: provides snippets for the snippet source
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+        { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+    },
+
+    version = '*',
+
+    opts = {
+        snippets = { preset = 'luasnip' },
+        -- Default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+    },
+    opts_extend = { "sources.default" }
+}
